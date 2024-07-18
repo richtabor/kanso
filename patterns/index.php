716e61424a7b65b27b14572cd9fc76bf918be93d
Rich Tabor <hi@richtabor.com>
2024-07-17 20:16:01 -0400

    formatting

--- patterns/index.php
+++ patterns/index.php
@@ -11,7 +11,7 @@
 <!-- wp:group {"tagName":"main","metadata":{"name":"Main"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
 <main class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:group {"metadata":{"name":"Title"},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":1,"className":"is-style-text-title"} -->
-<h1 class="wp-block-heading is-style-text-title"><?php esc_html_e('Hi, I\'m Rich Tabor', 'kanso');?></h1>
+<h1 class="wp-block-heading is-style-text-title"><?php esc_html_e( 'Hi, I\'m Rich Tabor', 'kanso' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:site-tagline /--></div>
