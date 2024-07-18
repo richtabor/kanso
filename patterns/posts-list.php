2139289bfb59eb6d9d23e46399997a4319939daa
Rich Tabor <hi@richtabor.com>
2024-07-18 12:22:18 -0400

    Use inherit on posts list

--- patterns/posts-list.php
+++ patterns/posts-list.php
@@ -20,7 +20,7 @@
 <p class="is-style-text-subtitle"><?php echo esc_html__( 'You may also enjoy…', 'kanso' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:query {"queryId":0,"query":{"perPage":"60","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false}} -->
+<!-- wp:query {"queryId":0,"query":{"perPage":"20","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
 <div class="wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"2px","bottom":"2px","left":"0px","right":"0px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group" style="padding-top:2px;padding-right:0px;padding-bottom:2px;padding-left:0px"><!-- wp:post-title {"isLink":true,"style":{"spacing":{"padding":{"top":"0px","bottom":"0px"}},"typography":{"lineHeight":"1.2"}},"className":"no-underline truncate","fontSize":"large"} /-->
