fa5da4981ecf93fcb7aa212acd71799dd4867728
Rich Tabor <hi@richtabor.com>
2024-08-13 12:29:37 -0400

    use core blocks for tools

--- patterns/cards-tools.php
+++ patterns/cards-tools.php
@@ -20,14 +20,53 @@
 		<p class="is-style-text-subtitle"><?php echo esc_html__( 'Tools', 'kanso' ); ?></p>
 	<!-- /wp:paragraph -->
 
-	<!-- wp:tabor/cards -->
-		<!-- wp:tabor/card {"title":"Screen Studio","link":"","description":"The best screen recorder."} /-->
-		<!-- wp:tabor/card {"title":"WordPress.com","link":"","description":"The best WordPress host."} /-->
-		<!-- wp:tabor/card {"title":"Typefully","link":"","description":"The best thread maker."} /-->
-		<!-- wp:tabor/card {"title":"Todoist","description":"The best todo app."} /-->
-		<!-- wp:tabor/card {"title":"Cleanshot","link":"","description":"The best screenshots."} /-->
-		<!-- wp:tabor/card {"title":"Raycast","link":"","description":"The best MacOS launcher."} /-->
-	<!-- /wp:tabor/cards -->
+	<!-- wp:group {"metadata":{"name":"<?php echo esc_html__( 'Tools', 'kanso' ); ?>"},"layout":{"type":"default"}} -->
+	<div class="wp-block-group"><!-- wp:paragraph {"className":"is-style-text-subtitle"} -->
+	<p class="is-style-text-subtitle"><?php echo esc_html__( 'Tools', 'kanso' ); ?></p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:group {"metadata":{"name":"Cards"},"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"12rem"}} -->
+	<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Card"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"className":"is-style-section-3","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group is-style-section-3" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"className":"no-underline","fontSize":"small"} -->
+	<p class="no-underline has-small-font-size"><a href="#"><?php echo esc_html__( 'Shaping WordPress', 'kanso' ); ?></a></p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"}},"className":"is-style-text-subtitle"} -->
+	<p class="is-style-text-subtitle" style="font-size:13px"><?php echo esc_html__( 'The best newsletter.', 'kanso' ); ?></p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"metadata":{"name":"Card"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"className":"is-style-section-3","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group is-style-section-3" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"className":"no-underline","fontSize":"small"} -->
+	<p class="no-underline has-small-font-size"><a href="#"><?php echo esc_html__( 'Rich\'s Blog', 'kanso' ); ?></a></p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"}},"className":"is-style-text-subtitle"} -->
+	<p class="is-style-text-subtitle" style="font-size:13px"><?php echo esc_html__( 'The best WordPress blog.', 'kanso' ); ?></p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"metadata":{"name":"Card"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"className":"is-style-section-3","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group is-style-section-3" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"className":"no-underline","fontSize":"small"} -->
+	<p class="no-underline has-small-font-size"><a href="#"><?php echo esc_html__( 'Using WordPress', 'kanso' ); ?></a></p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"}},"className":"is-style-text-subtitle"} -->
+	<p class="is-style-text-subtitle" style="font-size:13px"><?php echo esc_html__( 'The best WordPress tips.', 'kanso' ); ?></p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:group -->
+
+	<!-- wp:group {"metadata":{"name":"Card"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"blockGap":"0"},"layout":{"selfStretch":"fill","flexSize":null}},"className":"is-style-section-3","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group is-style-section-3" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"className":"no-underline","fontSize":"small"} -->
+	<p class="no-underline has-small-font-size"><a href="#"><?php echo esc_html__( 'Shaping WordPress', 'kanso' ); ?></a></p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:paragraph {"style":{"typography":{"fontSize":"13px"}},"className":"is-style-text-subtitle"} -->
+	<p class="is-style-text-subtitle" style="font-size:13px"><?php echo esc_html__( 'The best newsletter.', 'kanso' ); ?></p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:group --></div>
+	<!-- /wp:group --></div>
+	<!-- /wp:group -->
 
 	<!-- wp:spacer {"height":"var:preset|spacing|10"} -->
 		<div style="height:var(--wp--preset--spacing--10)" aria-hidden="true" class="wp-block-spacer"></div>
