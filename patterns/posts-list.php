c0bddd89c93da6a9fafee8f4e97ad78c6a640124
Rich Tabor <hi@richtabor.com>
2024-11-22 13:35:50 -0500

    Improvements based on accessibility feedback (#20)

--- patterns/posts-list.php
+++ patterns/posts-list.php
@@ -16,15 +16,15 @@
 <div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:paragraph {"className":"is-style-text-subtitle"} -->
-<p class="is-style-text-subtitle"><?php echo esc_html__( 'You may also enjoy…', 'kanso' ); ?></p>
-<!-- /wp:paragraph -->
+<!-- wp:heading {"className":"is-style-text-subtitle"} -->
+<h2 class="wp-block-heading is-style-text-subtitle"><?php echo esc_html__( 'You may also enjoy…', 'kanso' ); ?></h2>
+<!-- /wp:heading -->
 
 <!-- wp:query {"queryId":0,"query":{"perPage":"20","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true}} -->
 <div class="wp-block-query"><!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"2px","bottom":"2px","left":"0px","right":"0px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group" style="padding-top:2px;padding-right:0px;padding-bottom:2px;padding-left:0px"><!-- wp:post-title {"isLink":true,"style":{"spacing":{"padding":{"top":"0px","bottom":"0px"}},"typography":{"lineHeight":"1.2"}},"className":"no-underline truncate","fontSize":"large"} /-->
-
+<div class="wp-block-group" style="padding-top:2px;padding-right:0px;padding-bottom:2px;padding-left:0px">
+<!-- wp:post-title {"level":3,"isLink":true,"className":"no-underline truncate","style":{"spacing":{"padding":{"top":"0px","bottom":"0px"}},"typography":{"lineHeight":"1.2"}},"fontSize":"large"} /-->
 <!-- wp:post-date {"textAlign":"right","format":"M Y","style":{"typography":{"lineHeight":"1.2"}}} /--></div>
 <!-- /wp:group -->
 <!-- /wp:post-template --></div>
