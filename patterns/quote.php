f41d382b321fe1d46872c089eb36968dcc0d6731
Rich Tabor <hi@richtabor.com>
2024-11-22 13:43:17 -0500

    Create quote.php

---
+++ patterns/quote.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: Quote
+ * Slug: kanso/quote
+ * Categories: featured
+ * Viewport width: 520
+ *
+ * @package Kanso
+ */
+
+?>
+
+<!-- wp:group {"templateLock":"contentOnly","metadata":{"name":"Quote"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"var:preset|spacing|10"} -->
+<div style="height:var(--wp--preset--spacing--10)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"className":"is-style-section-3","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-group is-style-section-3" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--30)"><!-- wp:quote {"textAlign":"center","className":"is-style-plain"} -->
+<blockquote class="wp-block-quote has-text-align-center is-style-plain"><!-- wp:paragraph {"align":"center","className":"is-style-text-title","style":{"typography":{"fontSize":"1.3rem","fontStyle":"italic","fontWeight":"600","lineHeight":"1.3"}}} -->
+<p class="has-text-align-center is-style-text-title" style="font-size:1.3rem;font-style:italic;font-weight:600;line-height:1.3"><?php echo esc_html__( '"This nuanced understanding of taste defines the future of creativity."', 'kanso' ); ?></p>
+<!-- /wp:paragraph --><cite><?php echo esc_html__( 'Rich Tabor', 'kanso' ); ?></cite></blockquote>
+<!-- /wp:quote --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var:preset|spacing|10"} -->
+<div style="height:var(--wp--preset--spacing--10)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
